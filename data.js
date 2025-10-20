c2930a3b9b481f03577ac7c348cd3ec70f37b768
tonga <diazgaston.emm@gmail.com>
2025-10-20 17:26:37 -0300

    Algo de la entrega 3

---
+++ data.js
@@ -0,0 +1,37 @@
+export const medicosIniciales = [
+  {
+    id: 1,
+    nombre: "Marcelo Bielsa",
+    especialidad: "Cirujano",
+    obraSocial: "Italmédica",
+    foto: "img/doctor1.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Gerardo Martino",
+    especialidad: "Cardiología",
+    obraSocial: "Italmédica",
+    foto: "img/doctor2.jpg"
+  },
+  {
+    id: 3,
+    nombre: "Carla Bernardi",
+    especialidad: "Ortopedia",
+    obraSocial: "Italmédica",
+    foto: "img/doctor3.jpg"
+  },
+  {
+    id: 4,
+    nombre: "Damian Manso",
+    especialidad: "Endocrinólogo",
+    obraSocial: "Osde",
+    foto: "img/doctor4.jpg"
+  },
+  {
+    id: 5,
+    nombre: "Jorge Valdano",
+    especialidad: "Quinesiologo",
+    obraSocial: "Medicus",
+    foto: "img/doctor5.jpg"
+  }
+];
